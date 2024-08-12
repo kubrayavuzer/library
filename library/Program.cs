91f606ddd7ebe496a1fdf1d9615aba8f135c865a
kubra <kubra@DESKTOP-K1F6O3C>
2024-08-12 20:13:26 +0300

    Proje dosyası ekle.

---
+++ library/Program.cs
@@ -0,0 +1,21 @@
+﻿using library;
+ 
+public class Program
+{
+    static void Main()
+    {
+        //default constructor
+        book book1 = new book();
+        book1.BookTitle = "Aylin";
+        book1.AuthorName = "Ayşe";
+        book1.AuthorSurname = "Kulin";
+        book1.PublishingHouse = "Remzi Kitapevi";
+        book1.NumberOfPage = 398;
+        book1.PrintInformation();
+
+
+        //alternative constructor
+        book book2 = new book("Aylin", "Ayşe", "Kulin", "Remzi Kitapevi",  398);
+        book2.PrintInformation();
+    }
+}
---
+++ library/book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class book
+    {
+        //property
+        public string BookTitle { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public string PublishingHouse { get; set; }
+        public int NumberOfPage { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+
+        //default
+
+        public book()
+        {
+            RegistrationDate = DateTime.Now;
+            Console.WriteLine("book object created.");
+
+        }
+
+        //alternative
+
+        public book (string BookTitle, string AuthorName, string AuthorSurname, string PublishingHouse, int NumberOfPage)
+        {
+            BookTitle = BookTitle;
+            AuthorName = AuthorName;
+            AuthorSurname = AuthorSurname;
+            PublishingHouse = PublishingHouse;
+            NumberOfPage = NumberOfPage;
+            RegistrationDate = DateTime.Now;
+            Console.WriteLine("book object created.");
+
+        }
+
+        public void PrintInformation()
+
+        {
+            Console.WriteLine($" Book Title: {BookTitle}, Author Name: {AuthorName}, Author Surname: {AuthorSurname}, Publishing House: {PublishingHouse}, Number Of Page: {NumberOfPage}, Registration Date: {RegistrationDate}");
+        }
+        
+
+    }
+
+}
